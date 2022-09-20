4e128c9fbb2c1e2cc000c1e79d69ca4288bacab3
Newbiecodeweaver <carlosgilino@gmail.com>
2022-09-20 18:22:27 +0200

    route created for registration

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('players', [AuthController::class, 'register']); // crea un jugador/a
