9b4d5d655b7af945bcf2d19f98edf9ab2e8cced9
Newbiecodeweaver <carlosgilino@gmail.com>
2022-09-21 19:08:34 +0200

    api routes added

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\LoginController;
+use App\Http\Controllers\API\userController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,8 +17,23 @@ use App\Http\Controllers\API\AuthController;
 |
 */
 
+
+Route::post('players', [AuthController::class, 'register']); // crea un jugador/a
+Route::post('login', [LoginController::class, 'login']); // login jugador
+
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('players', [AuthController::class, 'register']); // crea un jugador/a
+
+// ->middleware('auth');
+
+Route::put('players/{id}', [userController::class, 'userChangeUsername']); // modifica el nom del jugador/a.
+Route::post('players/{id}/games/', [gameController::class, 'userRolldice']); // un jugador/a específic realitza una tirada dels daus.
+Route::delete('players/{id}/games', [gameController::class, 'removePlays']); // elimina les tirades del jugador/a. 
+Route::get('players', [userController::class, 'getUsersInfo']); // retorna el llistat de tots els jugadors/es del sistema amb el seu percentatge mitjà d’èxits 
+Route::get('players/{id}/games', [userController::class, 'getUserPlays']); // retorna el llistat de jugades per un jugador/a.
+Route::get('players/ranking', [userController::class, 'getUsersRanking']); // retorna el rànquing mitjà de tots els jugadors/es del sistema. És a dir, el percentatge mitjà d’èxits.
+Route::get('players/ranking/loser', [userController::class, 'getWorstUserRank']); // retorna el jugador/a amb pitjor percentatge d’èxit
+Route::get('players/ranking/winner', [userController::class, 'getBestUserRank']); // retorna el jugador/a amb millor percentatge d’èxit.
