d1f09a735825671616dec214a6b876835e2f72c8
Newbiecodeweaver <carlosgilino@gmail.com>
2022-10-08 20:48:43 +0200

    DatabaseSeeder edited for running factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        // \App\Models\partida::factory(10)->create();
     }
 }
